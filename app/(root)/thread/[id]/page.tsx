81a860040a6494900e051b5636dd66ede034e6f3
Kimginam97 <rlarlska79@naver.com>
2023-09-26 14:27:50 +0900

    add Reply/Comment

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -4,6 +4,7 @@ import { currentUser } from '@clerk/nextjs'
 import ThreadCard from '@/components/cards/ThreadCard'
 import { fetchUser } from '@/lib/actions/user.action'
 import { fetchThreadById } from '@/lib/actions/thread.actions'
+import Comment from '@/components/forms/Comment'
 
 export const revalidate = 0
 
@@ -31,6 +32,31 @@ async function page({ params }: { params: { id: string } }) {
           createdAt={thread.createdAt}
           comments={thread.children}
         />
+      </div>
+
+      <div className="mt-7">
+        <Comment
+          threadId={params.id}
+          currentUserImg={user.imageUrl}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user.id}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   )
